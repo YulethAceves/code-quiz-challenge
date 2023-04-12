8cfd19ba066220bbbfad4b6caa2dd3a39b635f46
Yuleth <yulethaceves@gmail.com>
2023-04-11 19:42:20 -0500

    created quiz questions and answers

--- script.js
+++ script.js
@@ -1,58 +1,113 @@
 const intro = document.getElementById('intro')
 const startBtn = document.getElementById('start-btn')
+const submitBtn = document.getElementById('submit')
 const quiz = document.getElementById('quiz')
 
-const question = document.getElementById('question')
-const answer = document.querySelectorAll('.answer')
-const a_text = document.getElementById('a_text')
-const b_text = document.getElementById('b_text')
-const c_text = document.getElementById('c_text')
-const d_text = document.getElementById('d_text')
-
-
-startBtn.addEventListener("click", startQuiz);
-
+const questionEl = document.getElementById('question')
+@@ -18, 6 + 19, 8 @@let score = 0
+// startQuiz()
 
 function startQuiz() {
-  const current
-}
+    quiz.style.display = "block";
+    intro.style.display = "none";
+    deselectAnswers()
+    const currentQuizData = quizData[currentQuiz]
+
+    @@ -44, 48 + 47, 67 @@function getSelected() {
+        return answer
+    }
+
+    submitBtn.addEventListener('click', () => {
+        const answer = getSelected()
+
+        if (answer) {
+            if (answer === quizData[currentQuiz].correct) {
+                score++
+            }
+
+            currentQuiz++
+
+            if (currentQuiz < quizData.length) {
+                loadQuiz()
+            } else {
+                quiz.innerHTML = `
+              <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+              <button onclick="location.reload()">Reload</button>
+          `
+            }
+        }
+    })
 
 
-
-const quizContent = [
-  {
-    question: "?",
-    a: "",
-    b: "",
-    c: "",
-    d: "",
-  },
-  {
-    question: "?",
-    a: "",
-    b: "",
-    c: "",
-    d: "",
-  },
-  {
-    question: "?",
-    a: "",
-    b: "",
-    c: "",
-    d: "",
-  },
-  {
-    question: "?",
-    a: "",
-    b: "",
-    c: "",
-    d: "",
-  },
-  {
-    question: "?",
-    a: "",
-    b: "",
-    c: "",
-    d: "",
-  },
-];
+    const quizData = [
+        {
+            question: "?",
+            a: "aaaa",
+            b: "bbbb",
+            c: "ccc",
+            d: "dddd",
+            correct: "a",
+            question: "What is the HTML tag is used when writing JavaScript code?",
+            a: "<javascript>",
+            b: "<javaScript>",
+            c: "<script>",
+            d: "<js>",
+            correct: "c",
+        },
+        {
+            question: "?",
+            a: "",
+            b: "",
+            c: "",
+            d: "",
+            correct: "",
+            question: "JavaScript is a ________ Side Scripting Language.",
+            a: "Server",
+            b: "Browser",
+            c: "Far",
+            d: "ISP",
+            correct: "b",
+        },
+        {
+            question: "?",
+            a: "",
+            b: "",
+            c: "",
+            d: "",
+            correct: "",
+            question: "Which built-in method sorts the elements of an array?",
+            a: "changeOrder(order)",
+            b: "order()",
+            c: "sort()",
+            d: "array_sorter",
+            correct: "c",
+        },
+        {
+            question: "?",
+            a: "",
+            b: "",
+            c: "",
+            d: "",
+            correct: "",
+            question: "Which of the following function of Array object reverses the order of the elements of an array?",
+            a: "reverse()",
+            b: "push()",
+            c: "reduce()",
+            d: "reuseRecycle()",
+            correct: "a",
+        },
+        {
+            question: "?",
+            a: "",
+            b: "",
+            c: "",
+            d: "",
+            correct: "",
+            question: "Which of the following is not a reserved word in JavaScript?",
+            a: "interface",
+            b: "throws",
+            c: "program",
+            d: "short",
+            correct: "c",
+        },
+    ];
